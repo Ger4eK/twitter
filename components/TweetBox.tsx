68d08a099c0e6efb3000b591b1a5512326a7f7a4
Ger4eK <ger4ek228@gmail.com>
2022-07-13 12:02:36 +0300

    implementing the image url functionality

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -5,15 +5,31 @@ import {
   PhotographIcon,
   SearchCircleIcon,
 } from '@heroicons/react/outline';
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const TweetBox = () => {
   const [input, setInput] = useState('');
+  const [image, setImage] = useState('');
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState(false);
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (!imageInputRef.current?.value) return;
+
+    setImage(imageInputRef.current.value);
+    imageInputRef.current.value = '';
+    setImageUrlBoxIsOpen(false);
+  };
 
   return (
     <div className='flex space-x-2 p-5'>
       <img
-        src='https://links.papareact.com/gll'
+        src='https://play-lh.googleusercontent.com/uh-YyABDPOU_NdZno8Eq11YkNu6BGNButL4YApda9rzc1YAHcLJyFYv7_yEy-s9Tbg'
         className='rounded-full mt-4 h-14 w-14 object-cover'
       />
       <div className='flex flex-1 items-center pl-2 '>
@@ -29,7 +45,10 @@ const TweetBox = () => {
           />
           <div className='flex items-center'>
             <div className='flex space-x-2 text-twitter flex-1 text-xl'>
-              <PhotographIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
+              <PhotographIcon
+                onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+                className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'
+              />
               <SearchCircleIcon className='h-5 w-5' />
               <EmojiHappyIcon className='h-5 w-5' />
               <CalendarIcon className='h-5 w-5' />
@@ -42,6 +61,32 @@ const TweetBox = () => {
               Tweet
             </button>
           </div>
+
+          {imageUrlBoxIsOpen && (
+            <form className=' mt-5 flex rounded-lg bg-twitter/80 py-2 px-4'>
+              <input
+                ref={imageInputRef}
+                type='text'
+                placeholder='Enter Image URL...'
+                className='flex-1 bg-transparent p-2 text-white outline-none placeholder-white'
+              />
+              <button
+                type='submit'
+                onClick={addImageToTweet}
+                className='font-bold text-white'
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+
+          {image && (
+            <img
+              src={image}
+              alt='Tweet Image'
+              className=' mt-10 h-40 w-full rounded-xl object-contain shadow-lg '
+            />
+          )}
         </form>
       </div>
     </div>
